b9e6dc71595d1f60c82529a44ad783fa7d62fa6d
artbaker82 <artbaker82@gmail.com>
2021-05-10 13:37:53 -0700

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Editor from "./components/editor";
+import Preview from "./components/preview";
+import "../src/styles/styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "#Header",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col">
+            <Editor input={this.state.input} onChange={this.handleChange} />
+          </div>
+
+          <div className="col">
+            <Preview value={this.state.input} />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
